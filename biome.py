28d5c09036fba17af280ceb224c180c4ed6c3c5b
BlueBandedHawk <gareth.litter@hotmail.co.uk>
2022-10-29 15:23:56 +0100

    Neater

--- biome.py
+++ biome.py
@@ -6,14 +6,14 @@ class Biome():
 
     def __init__(self, name, allowable_weathers, base_temp, base_humidity):
         """This function defines the spicific biome type
-        
+
         args: 
             name (str): the name of the biome type
             allowable_weathers (list(weather)): types of weather that could occur
             base_temp (int): temperature of biome in celcius
             base_humidity (int): humidity of the hex in percentage 
 
-            
+
         returns:
             (Biome): Initialised Biome object
         """
@@ -26,12 +26,12 @@ class Biome():
 
     def apply_weather_modifier(self, weather):
         """This function applies the weather modifiers from selected weather
-        
+
         args:
             current_temperature (int): the current temperature of the Hex
             current_humidity (int): the current humidity of the Hex
-        
-        
+
+
         returns:
             (Temperature): Modified temperature of Hex
             (Humidity): Modified humidity of Hex"""
@@ -42,4 +42,4 @@ class Biome():
 if __name__ == '__main__':
     test_hex = Biome("mountain", "list", 20, 50)
     test_hex.apply_weather_modifier("rain")
-
+    
