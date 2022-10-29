90273b230107cde066e9330140bff420d367d909
lowear <aaronrosslowe@gmail.com>
2022-10-29 14:23:43 +0100

    initiated hex.py

---
+++ hex.py
@@ -0,0 +1,37 @@
+""" This file contains a class to represent a hex on a map """
+
+
+class Hex():
+
+    def __init__(self, x_coord, y_coord, biome):
+        """ This function creates a new hex object
+        
+        Args:
+            x_coord (int): x coordinate of this hex
+            y_coord (int): y coordinate of this hex
+            biome (Biome): biome object that the hex is
+            
+        Returns:
+            (Hex): Initialised hex object
+        """
+        self.x = x_coord
+        self.y = y_coord
+        self.biome = biome
+        self.weather = None
+        self.weather_duration = 0
+
+    def get_humidity(self):
+        """ This function calculates the humidity of the hex
+        
+        Args:
+            None
+            
+        Returns:
+            (float): The humidity of the hex
+        """
+        raise NotImplementedError
+
+
+if __name__ == '__main__':
+    test_hex = Hex(1, 1, "test")
+    test_hex.get_humidity()
